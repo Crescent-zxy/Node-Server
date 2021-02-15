595bc73cfa8b0ba50fa643fc10415ddf085be1fc
zxy <358996085@qq.com>
2021-02-15 20:11:47 +0800

    AJAX封装

--- src/main.js
+++ src/main.js
@@ -1,91 +1,73 @@
-getCSS.onclick = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', '/style')
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const style = document.createElement('style')
-                style.innerHTML = request.response
-                document.head.appendChild(style)
-            }
-        }
-    }
-    request.send()
-}
-getHTML.onclick = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', '/2')
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const template = document.createElement('template')
-                template.innerHTML = request.response.trim()
-                document.body.appendChild(template.content.firstChild)
-            }
-        }
-    }
-    request.send()
-}
-getJS.onclick = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', '/3')
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const script = document.createElement('script')
-                script.innerHTML = request.response
-                document.body.appendChild(script)
-            }
-        }
-    }
-    request.send()
-}
-getXML.onclick = () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', '/4')
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const dom = request.responseXML
-                const text = dom.getElementsByTagName('warning')[0]
-                document.body.appendChild(text)
-            }
-        }
-    }
-    request.send()
-}
-getJSON.onclick = () => {
-    const request = new XMLHttpRequest
-    request.open('GET', '/5')
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const data = JSON.parse(request.response)
-                userName.innerText = data.name
-            }
-        }
-    }
-    request.send()
-}
-let n = 1
-getPage.onclick = () => {
-    const request = new XMLHttpRequest
-    request.open('GET', `/page${n+1}`)
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                const data = JSON.parse(request.response)
-                const elements = data.forEach(item => {
-                    const li = document.createElement('li')
-                    li.textContent = item.id
-                    xxx.appendChild(li);
-                })
-                n++
-                if (n === 3) {
-                    getPage.disabled = true
+/**封装 AJAX */
+const ajax = (method, url) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open(method, url)
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(null, request)
+                } else {
+                    reject.call(null, request)
                 }
             }
         }
-    }
-    request.send()
+        request.send()
+    })
+}
+/**封装 AJAX */
+
+getCSS.onclick = () => {
+    ajax('GET', '/style').then(request => {
+        const style = document.createElement('style')
+        style.innerHTML = request.response
+        document.head.appendChild(style)
+    })
+}
+
+getHTML.onclick = () => {
+    ajax('GET', '/2').then(request => {
+        const template = document.createElement('template')
+        template.innerHTML = request.response.trim()
+        document.body.appendChild(template.content.firstChild)
+    })
+}
+
+getJS.onclick = () => {
+    ajax('GET', '/3').then(request => {
+        const script = document.createElement('script')
+        script.innerHTML = request.response
+        document.body.appendChild(script)
+    })
+}
+
+getXML.onclick = () => {
+    ajax('GET', '/4').then(request => {
+        const dom = request.responseXML
+        const text = dom.getElementsByTagName('warning')[0]
+        document.body.appendChild(text)
+    })
+}
+
+getJSON.onclick = () => {
+    ajax('GET', '/5').then(request => {
+        const data = JSON.parse(request.response)
+        userName.innerText = data.name
+    })
+}
+
+let n = 1
+getPage.onclick = () => {
+    ajax('GET', `/page${n+1}`).then(request => {
+        const data = JSON.parse(request.response)
+        data.forEach(item => {
+            const li = document.createElement('li')
+            li.textContent = item.id
+            xxx.appendChild(li);
+        })
+        n++
+        if (n === 3) {
+            getPage.disabled = true
+        }
+    })
 }
